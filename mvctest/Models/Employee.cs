dcf7c2d4eb9433071bd917bd8ad04743fb2e5125
Jonathan Aldave Mendoza <jgam777@hotmail.com>
2024-11-12 09:43:17 -0500

    more c# code

---
+++ mvctest/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace mvctest.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            Id = 0;
+            Name = "Demo";
+            LastName = string.Empty;
+            Age = 0;
+            City = "Lima";
+            Country = "Peru";
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+    }
+}
